992a688a07461ab3ad0a36d0bb37bb7888200441
ecipil <erkncpl@gmail.com>
2017-03-03 16:50:30 +0300

    initial commit

---
+++ examples/basic.js
@@ -0,0 +1,224 @@
+'use strict';
+
+const fetch = require('node-fetch');
+const request = require('request');
+let Wit = null;
+let interactive = null;
+const client_id='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const siteHost ='https://osfglobalservices26-alliance-prtnr-eu03-dw.demandware.net';
+const siteSuffix = '/s/SiteGenesis/dw/shop/v17_2/product_search';
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/basic.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    var {text, quickreplies} = response;
+    /*console.log(response);
+    console.log('user said...', request.text);
+    console.log('sending...', JSON.stringify(response));
+    console.log(quickreplies);*/
+    if(quickreplies){
+        text = setQuickReplies(quickreplies,text);
+    }
+    console.log('request',JSON.stringify(request));
+    console.log('response',JSON.stringify(response));
+    //console.log(typeof(text)=='Object');
+  },
+  // You should implement your custom actions here
+  // See https://wit.ai/docs/quickstart
+  setIntentAndCategory(context){
+    console.log('setIntentAndCategory',JSON.stringify(context));
+   
+   setEntityValues(context,true);
+    /*var intentContext = context;
+    //intents array
+    var intents = intentContext.entities.intent;
+
+    console.log(intents[0].value);
+    //if intent is only one , we may check the confidence value
+    if(intents.length == 1){
+       setQuickReplies(intents[0].value,quickRepliesArray);
+    }*/
+  },
+  setGender(context,entities){
+    console.log('Set gender type : ',JSON.stringify(context));
+    console.log('Set gender type entity: ',JSON.stringify(entities));
+  },
+  productSearch(context,entities){
+    console.log('Product Search : ',JSON.stringify(context));
+    console.log('Product Search entity: ',JSON.stringify(entities));
+    setEntityValues(context,false);
+    console.log('Context Map: ',JSON.stringify(contextMap));
+    searchProductOnDemandWare();
+    
+  }
+};
+/**
+ * Search product on demand ware open api
+ * https://osfglobalservices26-alliance-prtnr-eu03-dw.demandware.net/s/SiteGenesis/dw/shop/v17_2/product_search?q=&refine_1=cgid=electronics-televisions-flat-screen&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&expand=images&count=10&sort=title&start=22
+ * @param {*} context 
+ * @param {*} reset 
+ */
+const searchProductOnDemandWare=()=> {
+  var entireURL = siteHost + siteSuffix;
+  var productSearchDirectory = '/product_search';
+  var q = contextMap['search-query']!="undefined" ? contextMap['search-query'] : '' ;
+  var refine_1 = 'cgid=' + extractCategory();
+  var expand ='images';
+  var count = 10;
+  var start = 0; 
+  var sort = 'title';
+  
+  entireURL = (entireURL+'?client_id=' + client_id + '&refine_1=' + refine_1 + '&q='+ q + '&expand=' + expand + '&count=' + count + '&start=' + start + '&sort=' + sort);
+  console.log(entireURL);
+  request(entireURL,
+  function(error,response,body){
+      if(!error  && response.statusCode == 200){
+        var bodyItem = JSON.parse(body);
+        var template =  prepareListTemplate(bodyItem.hits);
+        console.log(JSON.stringify(template));
+      }
+  });
+
+}
+//Update entity values 
+const setEntityValues =(context,reset) =>{
+  
+  if(reset == true){
+       contextMap = resetContextMap();
+  }
+   Object.keys(context.entities).forEach(function(key){
+        var tempKey = key.replace(/_/g,'-');
+        contextMap[tempKey] = context.entities[key][0].value;
+    });
+}
+const resetContextMap=()=>{
+  return {
+        "womens-clothing" : "",
+        "mens-clothing" : "",
+        "mens-accessories" :"",
+        "womens-accessories" :"",
+        "jewelry":"",
+        "search_query":"",
+        "electronics":""
+      };
+}
+var contextMap =  resetContextMap();
+//Extract category id 
+const extractCategory = ()=>{
+  var categoryid= '';
+  Object.keys(contextMap).forEach(function(key){
+       if(contextMap[key] !='' && (key!='search-query' && key!='intent')){
+          key = key.replace(/_/g,'-');
+          categoryid+= key +'-'+contextMap[key];
+       }
+       return categoryid;
+    });
+    return categoryid;
+}
+//Get the dynamic quick reply options from entity metadata
+const setQuickReplies = (quickReplyArray,actualText) => {
+  var qArray = [];
+  if(quickReplyArray){
+      quickReplyArray.forEach(function(v){
+            var item = {
+              "content_type" : "text",
+              "title" : v,
+              "payload" : v
+            };
+            qArray.push(item);
+      });
+    //FB quick reply template
+    //See the docs
+    //https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
+    var quickReplyResponse = {
+            "text" : actualText,
+            "quick_replies":[]
+          };
+    quickReplyResponse["quick_replies"]=qArray;
+    return quickReplyResponse;
+  }
+} 
+//Preapare facebook list template 
+const prepareListTemplate = (hits)=>{
+  var elementItem ={
+                    "title": "",
+                    "image_url": "",
+                    "subtitle": "",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": ""
+                    },
+                    "buttons": [
+                        {
+                            "title": "",
+                            "type": "web_url",
+                            "url": "",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": ""                        
+                        }
+                    ]                
+                };
+
+  var listTemplate = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": []
+        }
+      }
+  }
+  hits.forEach(function(item){
+      var productHit = item ;
+      var elementItem ={
+                    "title": "",
+                    "image_url": "",
+                    "subtitle": "",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": ""
+                    },
+                    "buttons": [
+                        {
+                            "title": "Add to Cart",
+                            "type": "web_url",
+                            "url": "",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": ""                        
+                        }
+                    ]                
+                };
+      elementItem.title = productHit.product_name;
+      elementItem.subtitle = productHit.image.title;
+      elementItem.image_url = productHit.image.link.replace(/\/large\//g,'/small/');
+      elementItem.default_action.url = productHit.link;
+      listTemplate.attachment.payload.elements.push(elementItem);
+  });
+  return listTemplate;
+}
+const client = new Wit({accessToken, actions});
+interactive(client);
---
+++ examples/messenger.js
@@ -0,0 +1,346 @@
+'use strict';
+
+// Messenger API integration example
+// We assume you have:
+// * a Wit.ai bot setup (https://wit.ai/docs/quickstart)
+// * a Messenger Platform setup (https://developers.facebook.com/docs/messenger-platform/quickstart)
+// You need to `npm install` the following dependencies: body-parser, express, request.
+//
+// 1. npm install body-parser express request
+// 2. Download and install ngrok from https://ngrok.com/download
+// 3. ./ngrok http 8445
+// 4. WIT_TOKEN=your_access_token FB_APP_SECRET=your_app_secret FB_PAGE_TOKEN=your_page_token node examples/messenger.js
+// 5. Subscribe your page to the Webhooks using verify_token and `https://<your_ngrok_io>/webhook` as callback URL.
+// 6. Talk to your bot on Messenger!
+
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const express = require('express');
+const fetch = require('node-fetch');
+//const request = require('request');
+
+let Wit = null;
+let log = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  log = require('../').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
+
+// Webserver parameter
+const PORT = process.env.PORT || 8445;
+
+// Wit.ai parameters
+const WIT_TOKEN = process.env.WIT_TOKEN;
+
+//Wit api url 
+const WIT_API_URL = 'https://api.wit.ai';
+
+
+// Messenger API parameters
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
+const FB_APP_SECRET = process.env.FB_APP_SECRET;
+if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
+
+let FB_VERIFY_TOKEN = null;
+FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+/*crypto.randomBytes(8, (err, buff) => {
+  if (err) throw err;
+  FB_VERIFY_TOKEN = buff.toString('hex');
+  console.log(`/webhook will accept the Verify Token "${FB_VERIFY_TOKEN}"`);
+});*/
+
+// ----------------------------------------------------------------------------
+// Messenger API specific code
+
+// See the Send API reference
+// https://developers.facebook.com/docs/messenger-platform/send-api-reference
+
+const fbMessage = (id, text) => {
+  const body = JSON.stringify({
+    recipient: { id },
+    message: (typeof(text)=='object' ? text : { text }),
+  });
+  console.log('FB Message Body');
+  console.log(body);
+  const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+  return fetch('https://graph.facebook.com/me/messages?' + qs, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body,
+  })
+  .then(rsp => rsp.json())
+  .then(json => {
+    if (json.error && json.error.message) {
+      throw new Error(json.error.message);
+    }
+    return json;
+  });
+};
+
+//----------------------------------------------------------------------
+//To be able to fetch entity metadata info
+// See the Wit.ai API reference 
+//https://wit.ai/docs/http/20160526#get--entities-:entity-id-link
+const getEntityMetadata = (entityId,callback) =>{
+  if(entityId){
+    var apiUrl = WIT_API_URL + '/entities/' + entityId;
+    var qs = 'v='+getLeadingZeroDate();   
+    var options = {
+        url     : apiUrl + '?'+ qs,
+        headers : {
+                  'Authorization': 'Bearer '+WIT_TOKEN
+                  }
+    };
+    //request(options,callback); 
+  }else{
+    return {};
+  }
+  
+};
+
+//Get the dynamic quick reply options from entity metadata
+const setQuickReplies = (quickReplyArray,actualText) => {
+  var qArray = [];
+  if(quickReplyArray){
+      quickReplyArray.forEach(function(v){
+            var item = {
+              "content_type" : "text",
+              "title" : v,
+              "payload" : v
+            };
+            qArray.push(item);
+      });
+    //FB quick reply template
+    //See the docs
+    //https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
+    var quickReplyResponse = {
+            "text" : actualText,
+            "quick_replies":[]
+          };
+    quickReplyResponse["quick_replies"]=qArray;
+    return quickReplyResponse;
+  }
+}
+
+
+//Get the date with leading zero 
+//eg. 03012017
+const getLeadingZeroDate = () => {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+  return year + (month < 10 ? '0' + month : month ) + (day < 10 ? '0' +day : day);
+}
+// ----------------------------------------------------------------------------
+// Wit.ai bot specific code
+
+// This will contain all user sessions.
+// Each session has an entry:
+// sessionId -> {fbid: facebookUserId, context: sessionState}
+const sessions = {};
+
+const findOrCreateSession = (fbid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user fbid
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].fbid === fbid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
+  if (!sessionId) {
+    // No session found for user fbid, let's create a new one
+    sessionId = new Date().toISOString();
+    sessions[sessionId] = {fbid: fbid, context: {}};
+  }
+  return sessionId;
+};
+
+//Response manipulated response, could be used for quick responses
+var quickRepliesArray= [];
+
+// Our bot actions
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    var {text, quickreplies} = response;
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid;
+
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      //text ='<a href="">abc</a>';
+      if(quickreplies){
+         text = setQuickReplies(quickreplies,text);
+      }
+      console.log(text);
+      return fbMessage(recipientId, text)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
+    } else {
+      console.error('Oops! Couldn\'t find user for session:', sessionId);
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
+  },
+  // You should implement your custom actions here
+  // See https://wit.ai/docs/quickstart
+  setIntentAndCategory(context){
+    console.log(context);
+    /*var intentContext = context;
+    //intents array
+    var intents = intentContext.entities.intent;
+
+    console.log(intents[0].value);
+    //if intent is only one , we may check the confidence value
+    if(intents.length == 1){
+       setQuickReplies(intents[0].value,quickRepliesArray);
+    }*/
+  },
+  setGender(context){
+    console.log('Set gender type : ',JSON.stringify(context));
+  },
+  productSearch(context){
+    console.log('Product Search : ',JSON.stringify(context));
+  }
+};
+
+// Setting up our bot
+const wit = new Wit({
+  accessToken: WIT_TOKEN,
+  actions,
+  logger: new log.Logger(log.INFO)
+});
+
+// Starting our webserver and putting it all together
+const app = express();
+app.use(({method, url}, rsp, next) => {
+  rsp.on('finish', () => {
+    console.log(`${rsp.statusCode} ${method} ${url}`);
+  });
+  next();
+});
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+
+// Webhook setup
+app.get('/webhook', (req, res) => {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+// Message handler
+app.post('/webhook', (req, res) => {
+  // Parse the Messenger payload
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const data = req.body;
+
+  if (data.object === 'page') {
+    data.entry.forEach(entry => {
+      entry.messaging.forEach(event => {
+        if (event.message && !event.message.is_echo) {
+          // Yay! We got a new message!
+          // We retrieve the Facebook user ID of the sender
+          const sender = event.sender.id;
+
+          // We retrieve the user's current session, or create one if it doesn't exist
+          // This is needed for our bot to figure out the conversation history
+          const sessionId = findOrCreateSession(sender);
+
+          // We retrieve the message content
+          const {text, attachments} = event.message;
+
+          if (attachments) {
+            // We received an attachment
+            // Let's reply with an automatic message
+            fbMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
+          } else if (text) {
+            // We received a text message
+
+            // Let's forward the message to the Wit.ai Bot Engine
+            // This will run all actions until our bot has nothing left to do
+            wit.runActions(
+              sessionId, // the user's current session
+              text, // the user's message
+              sessions[sessionId].context // the user's current session state
+            ).then((context) => {
+              // Our bot did everything it has to do.
+              // Now it's waiting for further messages to proceed.
+              console.log('Waiting for next user messages');
+
+              // Based on the session state, you might want to reset the session.
+              // This depends heavily on the business logic of your bot.
+              // Example:
+              // if (context['done']) {
+              //   delete sessions[sessionId];
+              // }
+
+              // Updating the user's current session state
+              sessions[sessionId].context = context;
+            })
+            .catch((err) => {
+              console.error('Oops! Got an error from Wit: ', err.stack || err);
+            })
+          }
+        } else {
+          console.log('received event', JSON.stringify(event));
+        }
+      });
+    });
+  }
+  res.sendStatus(200);
+});
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+app.listen(PORT);
+console.log('Listening on :' + PORT + '...');
