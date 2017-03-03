d61de8800eacb547f0ba984028342879c91e2eb1
ecipil <erkncpl@gmail.com>
2017-03-03 17:11:58 +0300

    Moved basic.js methods to messenger.js

--- examples/messenger.js
+++ examples/messenger.js
@@ -17,6 +17,10 @@ const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const express = require('express');
 const fetch = require('node-fetch');
+const request = require('request');
+const client_id='aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const siteHost ='https://osfglobalservices26-alliance-prtnr-eu03-dw.demandware.net';
+const siteSuffix = '/s/SiteGenesis/dw/shop/v17_2/product_search';
 //const request = require('request');
 
 let Wit = null;
@@ -82,61 +86,6 @@ const fbMessage = (id, text) => {
   });
 };
 
-//----------------------------------------------------------------------
-//To be able to fetch entity metadata info
-// See the Wit.ai API reference 
-//https://wit.ai/docs/http/20160526#get--entities-:entity-id-link
-const getEntityMetadata = (entityId,callback) =>{
-  if(entityId){
-    var apiUrl = WIT_API_URL + '/entities/' + entityId;
-    var qs = 'v='+getLeadingZeroDate();   
-    var options = {
-        url     : apiUrl + '?'+ qs,
-        headers : {
-                  'Authorization': 'Bearer '+WIT_TOKEN
-                  }
-    };
-    //request(options,callback); 
-  }else{
-    return {};
-  }
-  
-};
-
-//Get the dynamic quick reply options from entity metadata
-const setQuickReplies = (quickReplyArray,actualText) => {
-  var qArray = [];
-  if(quickReplyArray){
-      quickReplyArray.forEach(function(v){
-            var item = {
-              "content_type" : "text",
-              "title" : v,
-              "payload" : v
-            };
-            qArray.push(item);
-      });
-    //FB quick reply template
-    //See the docs
-    //https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
-    var quickReplyResponse = {
-            "text" : actualText,
-            "quick_replies":[]
-          };
-    quickReplyResponse["quick_replies"]=qArray;
-    return quickReplyResponse;
-  }
-}
-
-
-//Get the date with leading zero 
-//eg. 03012017
-const getLeadingZeroDate = () => {
-  var dateObj = new Date();
-  var month = dateObj.getUTCMonth() + 1; //months from 1-12
-  var day = dateObj.getUTCDate();
-  var year = dateObj.getUTCFullYear();
-  return year + (month < 10 ? '0' + month : month ) + (day < 10 ? '0' +day : day);
-}
 // ----------------------------------------------------------------------------
 // Wit.ai bot specific code
 
@@ -202,7 +151,9 @@ const actions = {
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
   setIntentAndCategory(context){
-    console.log(context);
+    console.log('setIntentAndCategory',JSON.stringify(context));
+   
+   setEntityValues(context,true);
     /*var intentContext = context;
     //intents array
     var intents = intentContext.entities.intent;
@@ -213,14 +164,208 @@ const actions = {
        setQuickReplies(intents[0].value,quickRepliesArray);
     }*/
   },
-  setGender(context){
+  setGender(context,entities){
     console.log('Set gender type : ',JSON.stringify(context));
+    console.log('Set gender type entity: ',JSON.stringify(entities));
   },
-  productSearch(context){
+  productSearch(context,entities){
     console.log('Product Search : ',JSON.stringify(context));
+    console.log('Product Search entity: ',JSON.stringify(entities));
+    setEntityValues(context,false);
+    console.log('Context Map: ',JSON.stringify(contextMap));
+    searchProductOnDemandWare();
+    
   }
 };
+//***************CUSTOM METHODS STARTS *************************/
+/**
+ * Search product on demand ware open api
+ * https://osfglobalservices26-alliance-prtnr-eu03-dw.demandware.net/s/SiteGenesis/dw/shop/v17_2/product_search?q=&refine_1=cgid=electronics-televisions-flat-screen&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&expand=images&count=10&sort=title&start=22
+ * @param {*} context 
+ * @param {*} reset 
+ */
+const searchProductOnDemandWare=()=> {
+  var entireURL = siteHost + siteSuffix;
+  var productSearchDirectory = '/product_search';
+  var q = contextMap['search-query']!="undefined" ? contextMap['search-query'] : '' ;
+  var refine_1 = 'cgid=' + extractCategory();
+  var expand ='images';
+  var count = 10;
+  var start = 0; 
+  var sort = 'title';
+  
+  entireURL = (entireURL+'?client_id=' + client_id + '&refine_1=' + refine_1 + '&q='+ q + '&expand=' + expand + '&count=' + count + '&start=' + start + '&sort=' + sort);
+  console.log(entireURL);
+  request(entireURL,
+  function(error,response,body){
+      if(!error  && response.statusCode == 200){
+        var bodyItem = JSON.parse(body);
+        if(typeof(bodyItem.hits)!="undefined"){
+          var template =  prepareListTemplate(bodyItem.hits);
+          console.log(template);
+        }else{
+          console.info('No product found!');
+        }
+        
+      }
+  });
 
+}
+//Update entity values 
+const setEntityValues =(context,reset) =>{
+  
+  if(reset == true){
+       contextMap = resetContextMap();
+  }
+   Object.keys(context.entities).forEach(function(key){
+        var tempKey = key.replace(/_/g,'-');
+        contextMap[tempKey] = context.entities[key][0].value;
+    });
+}
+const resetContextMap=()=>{
+  return {
+        "womens-clothing" : "",
+        "mens-clothing" : "",
+        "mens-accessories" :"",
+        "womens-accessories" :"",
+        "jewelry":"",
+        "search_query":"",
+        "electronics":""
+      };
+}
+var contextMap =  resetContextMap();
+//Extract category id 
+const extractCategory = ()=>{
+  var categoryid= '';
+  Object.keys(contextMap).forEach(function(key){
+       if(contextMap[key] !='' && (key!='search-query' && key!='intent')){
+          key = key.replace(/_/g,'-');
+          categoryid+= key +'-'+contextMap[key];
+       }
+       return categoryid;
+    });
+    return categoryid;
+}
+//Get the dynamic quick reply options from entity metadata
+const setQuickReplies = (quickReplyArray,actualText) => {
+  var qArray = [];
+  if(quickReplyArray){
+      quickReplyArray.forEach(function(v){
+            var item = {
+              "content_type" : "text",
+              "title" : v,
+              "payload" : v
+            };
+            qArray.push(item);
+      });
+    //FB quick reply template
+    //See the docs
+    //https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
+    var quickReplyResponse = {
+            "text" : actualText,
+            "quick_replies":[]
+          };
+    quickReplyResponse["quick_replies"]=qArray;
+    return quickReplyResponse;
+  }
+}
+//Preapare facebook list template 
+const prepareListTemplate = (hits)=>{
+  var elementItem ={
+                    "title": "",
+                    "image_url": "",
+                    "subtitle": "",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": ""
+                    },
+                    "buttons": [
+                        {
+                            "title": "",
+                            "type": "web_url",
+                            "url": "",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": ""                        
+                        }
+                    ]                
+                };
+
+  var listTemplate = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "list",
+            "top_element_style": "compact",
+            "elements": []
+        }
+      }
+  }
+  hits.forEach(function(item){
+      var productHit = item ;
+      var elementItem ={
+                    "title": "",
+                    "image_url": "",
+                    "subtitle": "",
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "",
+                        "messenger_extensions": true,
+                        "webview_height_ratio": "tall",
+                        "fallback_url": ""
+                    },
+                    "buttons": [
+                        {
+                            "title": "Add to Cart",
+                            "type": "web_url",
+                            "url": "",
+                            "messenger_extensions": true,
+                            "webview_height_ratio": "tall",
+                            "fallback_url": ""                        
+                        }
+                    ]                
+                };
+      elementItem.title = productHit.product_name;
+      elementItem.subtitle = productHit.image.title;
+      elementItem.image_url = productHit.image.link.replace(/\/large\//g,'/small/');
+      elementItem.default_action.url = productHit.link;
+      listTemplate.attachment.payload.elements.push(elementItem);
+  });
+  return listTemplate;
+}
+//Get the date with leading zero 
+//eg. 03012017
+const getLeadingZeroDate = () => {
+  var dateObj = new Date();
+  var month = dateObj.getUTCMonth() + 1; //months from 1-12
+  var day = dateObj.getUTCDate();
+  var year = dateObj.getUTCFullYear();
+  return year + (month < 10 ? '0' + month : month ) + (day < 10 ? '0' +day : day);
+}
+//----------------------------------------------------------------------
+//To be able to fetch entity metadata info
+// See the Wit.ai API reference 
+//https://wit.ai/docs/http/20160526#get--entities-:entity-id-link
+const getEntityMetadata = (entityId,callback) =>{
+  if(entityId){
+    var apiUrl = WIT_API_URL + '/entities/' + entityId;
+    var qs = 'v='+getLeadingZeroDate();   
+    var options = {
+        url     : apiUrl + '?'+ qs,
+        headers : {
+                  'Authorization': 'Bearer '+WIT_TOKEN
+                  }
+    };
+    //request(options,callback); 
+  }else{
+    return {};
+  }
+  
+};
+//***************CUSTOM METHODS END *************************/
 // Setting up our bot
 const wit = new Wit({
   accessToken: WIT_TOKEN,
